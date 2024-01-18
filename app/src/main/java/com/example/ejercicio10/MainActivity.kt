2d933050fc5a07de388cb446b429ce03cf223ea9
AlvaroMb <alvaro207mb@gmail.com>
2024-01-18 15:54:59 +0100

    ejercicio

--- app/src/main/java/com/example/ejercicio10/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio10/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.ejercicio10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(String(), "create");
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(String(), "start");
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(String(), "resume");
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(String(), "paused");
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(String(), "stopped");
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(String(), "destroyed");
+    }
+
 }
